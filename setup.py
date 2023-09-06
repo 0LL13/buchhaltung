744761b6dc74254844e786f98bc508468dbcd998
Oliver Stapel <hardy.ecc95@gmail.com>
2023-09-06 11:25:51 +0200

    add hatch and pyproject.toml

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/0LL13/buchhaltung",
-    version="0.0.4",
+    version="0.0.5",
     zip_safe=False,
     extras_require={
         "dev": ["check-manifest"],
