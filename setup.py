2099f13754be9d7ed0b0c440bbf0c16bbb862fb5
Oliver Stapel <hardy.ecc95@gmail.com>
2023-09-06 12:00:25 +0200

    new try, no changes, did I miss a step?

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/0LL13/buchhaltung",
-    version="0.0.8",
+    version="0.0.9",
     zip_safe=False,
     extras_require={
         "dev": ["check-manifest"],
